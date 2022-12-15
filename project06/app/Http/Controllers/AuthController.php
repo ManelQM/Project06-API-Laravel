e1e655dce172dc2edd1120b76a27c58d3ba1bf0a
Manel <quemonmanel@gmail.com>
2022-12-16 00:01:00 +0100

    feat:authcontroller-jwt-register-login

--- project06/app/Http/Controllers/AuthController.php
+++ project06/app/Http/Controllers/AuthController.php
@@ -46,8 +46,18 @@ class AuthController extends Controller
             return response()->json([
                 'success' => false,
                 'message' => 'Invalid Email or Password',
-            ]);
+            ], Response::HTTP_UNAUTHORIZED);
         }
+        return response() -> json([
+            'success' => true,
+            'token' => $jwt_token,
+        ]);
+
+    }
+    public function profile (){
+        return response() -> json([
+            'user' => auth()->user()
+        ]);
     }
 }
 
