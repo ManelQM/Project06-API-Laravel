6ae67dfb6874e7e43c29349d5579a5e54eb87cad
Manel <quemonmanel@gmail.com>
2022-12-16 01:27:56 +0100

    chore:seeders,models,controllers names,fields mods and similar stuff

--- project06/database/migrations/2022_12_14_214113_create_users_table.php
+++ project06/database/migrations/2022_12_14_214113_create_users_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name'); 
+            $table->string('email');
+            $table->string('password');
             $table->timestamps();
         });
     }
