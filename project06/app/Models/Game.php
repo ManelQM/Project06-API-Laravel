5aad1dfc8a1cd4f6fe23b68d98f41af5625e202c
Manel <quemonmanel@gmail.com>
2022-12-18 00:17:03 +0100

    feat:game model

--- project06/app/Models/Game.php
+++ project06/app/Models/Game.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+
+    public function user() {
+        return $this -> belongsTo(User::class);
+    }
+
+    public function parties(){
+        return $this ->hasMany(Party::class);
+    }
+    
 }
+
